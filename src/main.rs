5ffe8a2d700835553d92bfb7b2ac7c4f0001e0af
srbdev <sylvain.rb@gmail.com>
2020-06-08 15:35:44 -0600

    Update help message + some cleanup

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,20 @@ use filetime::{FileTime, set_file_atime, set_file_mtime};
 #[derive(StructOpt)]
 struct Cli {
     // TODO: add additional args from the man pages
-    // -d, --date=STRING: parse STRING and use it instead of current time
-    // -f: (ignored)
-    // -h, --no-deference: affect each symbolic link instead of any referenced file (only useful on
-    // systems that can change the timestamp of a symlink)
-    // -r, --reference=FILE: use this file's time instead of current time
-    // -t STAMP: use [[CC]YY]MMDDhhmm[.ss] instead of current time
-    // --time=WORD: change the specified time: WORD is access, atime, or use: equivalent to -a WORD
-    // is modify or mtime: equivalent to -m
-    //
-    // Note that the -d and -t options accept different time-date formats.
-    //
-    // Date String:
-    // The --date=STRING is a mostly free format human readable date string such as "Sun, 29 Feb
-    // 2004 16:21:42 -0800" or "2004-02-29 16:21:42" or even "next Thursday". A date string may
-    // contain items indicating calendar date, time of day, time zone, day of week, relative time,
-    // relative date, and numbers. An empty string indicates the beginning of the day. The date
-    // string format is more complex than is easily documented here but is fully described in the
-    // info documentation.
-    //
     // structopt docs: https://docs.rs/crate/structopt/0.3.13
 
-    #[structopt(short = "a", long)]
+    /// Change only the access time
+    #[structopt(short = "a")]
     only_atime: bool,
-
-    #[structopt(short = "m", long)]
-    only_mtime: bool,
-
+    /// Do not create any files
     #[structopt(short = "c", long)]
     no_create: bool,
+    /// (ignored)
+    #[structopt(short = "f")]
+    force: bool,
+    /// Change only the modification time
+    #[structopt(short = "m")]
+    only_mtime: bool,
 
     #[structopt(name = "FILE", parse(from_os_str))]
     files: Vec<PathBuf>,
